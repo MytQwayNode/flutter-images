21b5f16df8cf0e8a56e301f89902c625fbf23de3
MytQwayNode <myt.qway.node@gmail.com>
2020-07-08 11:05:46 -0600

    with dropdown and input field

--- lib/main.dart
+++ lib/main.dart
@@ -31,9 +31,11 @@ class _ImageCaptureState extends State<ImageCapture> {
   /// Active image file
   File _imageFile;
   final picker = ImagePicker();
+
   /// Select an image via gallery or camera
   Future<void> _pickImage(ImageSource source) async {
-    PickedFile selected = await picker.getImage(source: source, maxHeight: 240, maxWidth: 320);
+    PickedFile selected =
+        await picker.getImage(source: source, maxHeight: 240, maxWidth: 320);
 
     setState(() {
       _imageFile = File(selected.path);
@@ -42,9 +44,7 @@ class _ImageCaptureState extends State<ImageCapture> {
 
   /// Cropper plugin
   Future<void> _cropImage() async {
-    File cropped = await ImageCropper.cropImage(
-        sourcePath: _imageFile.path
-    );
+    File cropped = await ImageCropper.cropImage(sourcePath: _imageFile.path);
 
     setState(() {
       _imageFile = cropped ?? _imageFile;
@@ -61,29 +61,29 @@ class _ImageCaptureState extends State<ImageCapture> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-     bottomNavigationBar: BottomAppBar(
-       child: Row(
-         children: <Widget>[
-           IconButton(
-             icon: Icon(Icons.photo_camera),
-             onPressed: () {
-               _clear();
-               return _pickImage(ImageSource.camera);
-             },
-           ),
-           IconButton(
-             icon: Icon(Icons.photo_library),
-             onPressed: () {
-               _clear();
-               return _pickImage(ImageSource.gallery);
-             },
-           )
-         ],
-       ),
-     ),
+      bottomNavigationBar: BottomAppBar(
+        child: Row(
+          children: <Widget>[
+            IconButton(
+              icon: Icon(Icons.photo_camera),
+              onPressed: () {
+                _clear();
+                return _pickImage(ImageSource.camera);
+              },
+            ),
+            IconButton(
+              icon: Icon(Icons.photo_library),
+              onPressed: () {
+                _clear();
+                return _pickImage(ImageSource.gallery);
+              },
+            )
+          ],
+        ),
+      ),
       body: ListView(
         children: <Widget>[
-          if(_imageFile !=null) ...[
+          if (_imageFile != null) ...[
             Image.file(_imageFile),
             Row(
               children: <Widget>[
@@ -114,62 +114,102 @@ class Uploader extends StatefulWidget {
 }
 
 class _UploaderState extends State<Uploader> {
-  final FirebaseStorage _storage = FirebaseStorage(storageBucket: 'gs://flutterquotesapp.appspot.com/');
+  final FirebaseStorage _storage =
+      FirebaseStorage(storageBucket: 'gs://flutterquotesapp.appspot.com/');
 
   StorageUploadTask _uploadTask;
 
-  void _startUpload() async {
-      String filePath = 'images/${DateTime.now()}.png';
+  String dropdownValue;
+  List<String> _categories = ['Alyssa', 'Beloha', 'Cathy', 'Daddy'];
 
-      setState(() {
-        _uploadTask = _storage.ref().child(filePath).putFile(widget.file);
-      });
+  void _startUpload(String filename) async {
+    String filePath = 'images/$filename${DateTime.now()}.png';
+
+    setState(() {
+      _uploadTask = _storage.ref().child(filePath).putFile(widget.file);
+    });
 
     var storageTaskSnapshot = await _uploadTask.onComplete;
     var downloadUrl = await storageTaskSnapshot.ref.getDownloadURL();
     debugPrint(downloadUrl);
-      Firestore.instance.collection("images").add({
-        "category": "flower",
-        "name": "rose",
-        "imageUrl": downloadUrl
-      }).then((response) {
-        print(response.documentID);
-      }).catchError((error) {
-        print(error);
-      });
+    Firestore.instance.collection("images").add({
+      "category": "flower",
+      "name": "rose",
+      "imageUrl": downloadUrl
+    }).then((response) {
+      print(response.documentID);
+    }).catchError((error) {
+      print(error);
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    if ( _uploadTask != null) {
+    if (_uploadTask != null) {
       return StreamBuilder<StorageTaskEvent>(
-        stream: _uploadTask.events,
-        builder: (context, snapshot) {
-          var event = snapshot?.data?.snapshot;
+          stream: _uploadTask.events,
+          builder: (context, snapshot) {
+            var event = snapshot?.data?.snapshot;
 
-          double progressPercent = event != null ? event.bytesTransferred / event.totalByteCount:0;
+            double progressPercent = event != null
+                ? event.bytesTransferred / event.totalByteCount
+                : 0;
 
-          return Column(
-            children: <Widget>[
-              if(_uploadTask.isComplete)
-                Text('Uploaded!'),
-              if(_uploadTask.isPaused)
-                FlatButton(child: Icon(Icons.play_arrow),
-                onPressed: _uploadTask.resume,),
-              if(_uploadTask.isInProgress)
-                FlatButton(child: Icon(Icons.pause),onPressed: _uploadTask.pause,),
-              LinearProgressIndicator(value: progressPercent),
-              Text(
-                '${(progressPercent * 100).toStringAsFixed(2)} % '
-              )
-            ],
-          );
-        });
+            return Column(
+              children: <Widget>[
+                if (_uploadTask.isComplete) Text('Uploaded!'),
+                if (_uploadTask.isPaused)
+                  FlatButton(
+                    child: Icon(Icons.play_arrow),
+                    onPressed: _uploadTask.resume,
+                  ),
+                if (_uploadTask.isInProgress)
+                  FlatButton(
+                    child: Icon(Icons.pause),
+                    onPressed: _uploadTask.pause,
+                  ),
+                LinearProgressIndicator(value: progressPercent),
+                Text('${(progressPercent * 100).toStringAsFixed(2)} % ')
+              ],
+            );
+          });
     } else {
-      return FlatButton.icon(onPressed: _startUpload, icon: Icon(Icons.cloud_upload), label: Text('Upload to Firebase'));
+      TextEditingController filenameInputController = TextEditingController();
+
+
+      return Container(
+          child: Column(
+            children: <Widget>[
+              DropdownButton(
+                hint: Text("Please choose a category"),
+                value: dropdownValue,
+                items: _categories.map((location) {
+                  return DropdownMenuItem(
+                    child: new Text(location),
+                    value: location,
+                  );
+                }).toList(),
+                onChanged: (newValue){
+                  setState(() {
+                    dropdownValue = newValue;
+                  });
+                  debugPrint(dropdownValue);
+                },
+              ),
+              TextField(
+                controller: filenameInputController,
+                decoration: InputDecoration(
+                  labelText: "Filename:",
+                ),
+              ),
+              FlatButton.icon(
+                  onPressed: () => _startUpload(filenameInputController.text),
+                  icon: Icon(Icons.cloud_upload),
+                  label: Text('Upload to Firebase')),
+            ],
+          ));
     }
 
     return Container();
   }
 }
-
